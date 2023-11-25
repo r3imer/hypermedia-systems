1e652b9301b1e4d22c6f6004632e0ede0ed5b6ed
Philipp Reimer <philipp.reimer93@gmail.com>
2023-11-25 19:15:53 -0300

    add mvc template

---
+++ src/Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Reim.Htmx.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
