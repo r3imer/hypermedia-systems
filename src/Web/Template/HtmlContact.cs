732353ba15e255f2a76ffcdf93aa97c3786cedbf
r3imer <philipp.reimer93@gmail.com>
2024-01-04 21:47:47 -0300

    fix: remove unused code

--- src/Web/Template/HtmlContact.cs
+++ src/Web/Template/HtmlContact.cs
@@ -74,17 +74,4 @@ public static partial class Template {
         </p>
         """;
 
-    public static string HtmlRow(this Contact x) => $$"""
-        <tr>
-            <td>{{ x.first }}</td>
-            <td>{{ x.last }}</td>
-            <td>{{ x.phone }}</td>
-            <td>{{ x.email }}</td>
-            <td>
-                <a href="/contacts/{{ x.id }}/edit">Edit</a>
-                <a href="/contacts/{{ x.id }}">View</a>
-            </td>
-        </tr>
-        """;
-
 }
