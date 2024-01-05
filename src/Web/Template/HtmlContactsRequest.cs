2202506fef46710f5787c06f1260a2c36e15ba8e
r3imer <philipp.reimer93@gmail.com>
2024-01-04 21:48:15 -0300

    add delete link

--- src/Web/Template/HtmlContactsRequest.cs
+++ src/Web/Template/HtmlContactsRequest.cs
@@ -46,6 +46,9 @@ public static partial class Template {
             <td>
                 <a href="/contacts/{{ b.id }}/edit">Edit</a>
                 <a href="/contacts/{{ b.id }}">View</a>
+                <a href="#" hx-delete="/contacts/{{ b.id }}"
+                   hx-confirm="Are you sure you want to delete this contact?"
+                   hx-target="body">Delete</a>
             </td>
         </tr>
         """).Join() + ( a.arr.Length == Const.PAGE_SIZE ? $$"""
